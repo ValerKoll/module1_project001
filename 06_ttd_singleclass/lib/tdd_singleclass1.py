9b439ea351d11b6b35a9e2b32fc68782ddb6aaf4
ValerKoll <valerio.uk@gmail.com>
2023-11-20 15:43:35 +0000

    Design single class exercise - completed

---
+++ 06_ttd_singleclass/lib/tdd_singleclass1.py
@@ -0,0 +1,41 @@
+class Tasks():
+    def __init__(self):
+       self.tasks_dict = {}
+       self.__index = 0
+       
+    def add_tasks(self, title_to_add, content_to_add):
+        if title_to_add == "":
+            title_to_add = "Untitled"
+        if content_to_add == "":
+            return None
+        #index = int(list(self.tasks_dict)[-1])
+        #self.tasks_dict[str(index+1)] = f"{title_to_add.capitalize()}: {content_to_add.lower()}"
+        self.__index += 1
+        self.tasks_dict[str(self.__index)] = {'Title':title_to_add, 'content':content_to_add, 'completed':False}
+        return "Entry Added"
+    
+    def get_tasks(self):
+        if not len(self.tasks_dict):
+            return "List Empty"
+        text_to_print = "\033[0;31m==== TASK NOT COMPLETED ====\n"
+        text_to_print += ''.join([
+            (f"#{key} - {item['Title'].upper()}: {item['content']}\n") for key, item in self.tasks_dict.items() if item['completed']==False])
+        text_to_print += "\033[0;36m==== TASK COMPLETED ====\n"
+        text_to_print += ''.join([
+            (f"#{key} - {item['Title'].upper()}: {item['content']}\n") for key, item in self.tasks_dict.items() if item['completed']==True])
+        return text_to_print
+    
+    def flag_completed(self, task_id):
+        self.tasks_dict[str(task_id)]['completed'] = True
+    
+t = Tasks()
+t.add_tasks("Wsdsd", "Wsdsd")
+t.add_tasks("Ws", "skdlsjdlkjsld;")
+t.add_tasks("Not", "skdlsjdlkjsld;")
+t.add_tasks("YES", "skdlsjdlkjsld;")
+t.add_tasks("But", "skdlsjdlkjsld;")
+print(t.get_tasks())
+t.flag_completed(2)
+print(t.get_tasks())
+t.flag_completed(4)
+print(t.get_tasks())
---
+++ 06_ttd_singleclass/tests/test_tdd_singleclass1.py
@@ -0,0 +1,38 @@
+from lib.tdd_singleclass1 import *
+
+def test_given_empty_paramenters_return_none():
+    task = Tasks()
+    result = task.add_tasks("", "")
+    assert result == None
+    result = task.add_tasks("Test", "")
+    assert result == None
+
+
+def test_given_correct_paramenters_return_string():
+    task = Tasks()
+    result = task.add_tasks("", "Test")
+    assert result == "Entry Added"
+    result = task.add_tasks("Title", "Content ONE")
+    assert result == "Entry Added"
+    result = task.get_tasks()
+    assert result == ["Untitled: test", "Title: content one"]
+
+def test_given_no_tasks_return_message_empty():
+    task = Tasks()
+    task.add_tasks("Title", "")
+    task.add_tasks("", "")
+    result = task.get_tasks()
+    assert result == "List Empty"
+    
+    
+def test_given_tasks_return_number_correct_entries():
+    task = Tasks()
+    task.add_tasks("Title", "Content ONE")
+    task.add_tasks("Title", "Content ONE")
+    task.add_tasks("Title", "Content ONE")
+    result = task.get_tasks()
+    assert len(result) == 3
+    task.add_tasks("Title", "")
+    task.add_tasks("Title", "Content ONE")
+    result = task.get_tasks()
+    assert len(result) == 4
